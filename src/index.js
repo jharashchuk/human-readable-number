fe3582969f628c091cb66050a6976d4449131195
jharashchuk <j.garaschuk@gmail.com>
2022-01-08 22:53:59 +0100

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
-module.exports = function toReadable (number) {
-  
-}
+module.exports = function toReadable (number) {    
+    let ones = ['zero', 'one', 'two', 'three', 'four',
+            'five', 'six', 'seven', 'eight', 'nine',
+            'ten', 'eleven', 'twelve', 'thirteen', 'fourteen',
+            'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];   
+    let tens = ['twenty','thirty', 'forty','fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+    let rest = 0;   
+    if (number < 20) return ones[number];   
+    if (number < 100) {     
+        rest = number % 10; 
+        if (rest === 0) {
+            return tens[Math.floor(number/10)-2]; 
+        } else {   
+        return tens[Math.floor(number/10)-2] + " " + (rest > 0 ? ones[rest] : "");   
+        }
+    }
+    if (number < 1000) {   
+        rest = number % 100;
+        if (rest === 0) {
+            return ones[Math.floor(number/100)] + " hundred"; 
+        } else {
+        return ones[Math.floor(number/100)] + " hundred " + (number % 100 > 0 ? toReadable(number % 100) : "");  
+        } 
+    }   
+};
